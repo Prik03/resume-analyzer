c4880bc6068622b0ad149b18e1fac4abfaf47e09
Prik03 <princesain9319@gmail.com>
2026-05-10 01:04:06 +0530

    Frontend UI part is completed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/component/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Resume Analyzer" },
+    { name: "description", content: "Get Your Dream Job with AI Analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-5">
+          <h1>Ai Resume Analyzer Application</h1>
+          <h2>Review your submission and check AI powered Feedback</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resumeData={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
